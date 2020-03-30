94a07f088a6b7dc019c08c6048765b0df2bc67bc
shodayuki <yshoda1227@gmail.com>
2020-03-30 09:08:49 +0900

    Create Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Form from './components/Form/Form';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+    	<h1 className="App-title">Todoアプリ</h1>
+    	<Form />
     </div>
   );
 }
