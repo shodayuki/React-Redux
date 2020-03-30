a494be401e5970b735aa600146480e53492247ee
shodayuki <yshoda1227@gmail.com>
2020-03-30 09:34:29 +0900

    Create Filter.jsx and Filter.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form/Form';
+import Filter from './components/Filter/Filter';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
     	<h1 className="App-title">Todoアプリ</h1>
     	<Form />
+      <Filter />
     </div>
   );
 }
